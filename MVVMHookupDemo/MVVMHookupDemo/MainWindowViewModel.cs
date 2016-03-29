fc1f8e9cd752c5d2bad5d03add6d1d6e23b871e8
Jonathan Bachelor <Jonathan.Bachelor@hunterindustries.com>
2016-03-29 08:33:05 -0700

    Added notifications to the top of the main window, which can be dismissed by clicking on them by way of a dependency object and a new class inheriting from Behavior<T>. Pretty cool shit, though I have trouble totally understanding how this is working. This commit marks the end of the module, 'View/ViewModel Communication in WPF'.

--- MVVMHookupDemo/MVVMHookupDemo/MainWindowViewModel.cs
+++ MVVMHookupDemo/MVVMHookupDemo/MainWindowViewModel.cs
@@ -1,19 +1,44 @@
 ﻿using MVVMHookupDemo.Customers;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace MVVMHookupDemo
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private Timer timer = new Timer(5000);
+        
         public MainWindowViewModel()
         {
             CurrentViewModel = new CustomerListViewModel();
+            timer.Elapsed += (s, e) => NotificationMessage = "At the tone the time will be: " + DateTime.Now.ToLocalTime();
+            timer.Start();
         }
 
         public Object CurrentViewModel { get; set; }
+
+        private string notificationMessage;
+        public string NotificationMessage
+        {
+            get
+            {
+                return notificationMessage;
+            }
+            set
+            {
+                if(value != notificationMessage)
+                {
+                    notificationMessage = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("NotificationMessage"));
+                }
+            }
+        }
     }
 }
---
+++ MVVMHookupDemo/MVVMHookupDemo/ShowNotificationMessageBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MVVMHookupDemo
+{
+    public class ShowNotificationMessageBehavior : Behavior<ContentControl>
+    {
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Message.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ShowNotificationMessageBehavior), new PropertyMetadata(null, OnMessageChanged));
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (ignoredObject, mouseButtonEventArgs) =>
+                AssociatedObject.Visibility = Visibility.Collapsed;
+        }
+
+        private static void OnMessageChanged(DependencyObject dependencyObj, DependencyPropertyChangedEventArgs args)
+        {
+            var behavior = ((ShowNotificationMessageBehavior)dependencyObj);
+            behavior.AssociatedObject.Content = args.NewValue;
+            behavior.AssociatedObject.Visibility = Visibility.Visible;
+        }
+    }
+}

ce147d92b8184612b7ef93bb047edf04ccb7dfd8
Jonathan Bachelor <Jonathan.Bachelor@hunterindustries.com>
2016-04-07 08:34:39 -0700

    Completion of module 5 'Demo: View Switching Navigation'.

---
+++ ZzaDesktop/ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDesktop.Customers;
+using ZzaDesktop.OrderPrep;
+using ZzaDesktop.Orders;
+
+namespace ZzaDesktop
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            NavCommand = new RelayCommand<string>(OnNav);
+        }
+
+        private CustomerListViewModel customerListViewModel = new CustomerListViewModel();
+        private OrderPrepViewModel orderPrepViewModel = new OrderPrepViewModel();
+
+        private OrderViewModel orderViewModel = new OrderViewModel();
+
+        public RelayCommand<string> NavCommand { get; private set; }
+
+        private BindableBase currentViewModel;
+        public BindableBase CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set { SetProperty(ref currentViewModel, value); }
+        }
+        
+        private void OnNav(string destination)
+        {
+            switch (destination)
+            {
+                case "orderPrep":
+                    CurrentViewModel = orderPrepViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = customerListViewModel;
+                    break;
+            }
+        }
+    }
+}
--- ZzaDesktop/ZzaDesktop/RelayCommand.cs
+++ ZzaDesktop/ZzaDesktop/RelayCommand.cs
@@ -53,21 +53,49 @@ namespace ZzaDesktop
     public class RelayCommand<T> : ICommand
     {
         private Action<T> targetExecuteMethod;
+        private Func<T, bool> targetCanExecuteMethod;
+
+        public RelayCommand(Action<T> executeMethod)
+        {
+            targetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            targetExecuteMethod = executeMethod;
+            targetCanExecuteMethod = canExecuteMethod;
+        }
 
         #region ICommand Members
 
-        public event EventHandler CanExecuteChanged;
+        // Beware - should use weak references if command instance lifetime is longer than lifetime of UI objects that get hooked up to command
+        // Prism commands solve this in their implementation
+        public event EventHandler CanExecuteChanged = delegate { };
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            if (targetCanExecuteMethod != null)
+            {
+                T tparm = (T)parameter;
+                return targetCanExecuteMethod(tparm);
+            }
+            if (targetExecuteMethod != null)
+                return true;
+
+            return false;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            if (targetExecuteMethod != null)
+                targetExecuteMethod((T)parameter);
         }
 
         #endregion ICommand Members
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
     }
 }

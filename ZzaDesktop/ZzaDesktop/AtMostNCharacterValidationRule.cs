e78c625d665eb241f4735b159e3202bbc2232bb7
Jonathan Bachelor <Jonathan.Bachelor@hunterindustries.com>
2016-04-20 08:36:38 -0700

    Added a second validation rule mandating a max number of chars for a string input, adding it to the same search field that the minimum character count rule was applied to. Both rules operate. Note that these rules execute synchronously, so it would not be great if the rule needed to go query a service or something long-running.

---
+++ ZzaDesktop/ZzaDesktop/AtMostNCharacterValidationRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ZzaDesktop
+{
+    public class AtMostNCharacterValidationRule : ValidationRule
+    {
+        public int MaximumStringLength { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value.GetType() != typeof(string))
+                throw new ArgumentException("AtMostNCharacterValidationRule is meant only to validate a string argument.");
+
+            string valueString = value.ToString();
+
+            if (valueString.Length > MaximumStringLength)
+                return new ValidationResult(false, $"Cannot be longer than {MaximumStringLength} characters");
+            else
+                return new ValidationResult(true, null);
+        }
+    }
+}

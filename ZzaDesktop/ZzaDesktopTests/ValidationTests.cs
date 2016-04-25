ff6e1e901aebadba7ee012ea26faaa76c9dbea35
Jonathan Bachelor <Jonathan.Bachelor@hunterindustries.com>
2016-04-25 08:51:47 -0700

    Added unit tests for completeness in testing AtLeastNCharacterValidationRule.

--- ZzaDesktop/ZzaDesktopTests/ValidationTests.cs
+++ ZzaDesktop/ZzaDesktopTests/ValidationTests.cs
@@ -10,7 +10,7 @@ namespace ZzaDesktopTests
     public class ValidationTests
     {
         [TestMethod]
-        public void AtLeastNCharacterValidationRulePassesWithEmptyStringIfZeroCharsIsValid()
+        public void AtLeastNCharacterValidationRulePassesWithEmptyStringWhenZeroCharsIsValid()
         {
             var validationRule = new AtLeastNCharacterValidationRule()
             {
@@ -23,7 +23,7 @@ namespace ZzaDesktopTests
         }
 
         [TestMethod]
-        public void AtLeastNCharacterValidationRuleFailsWithEmptyString()
+        public void AtLeastNCharacterValidationRuleFailsWithEmptyStringWhenZeroCharsIsInvalid()
         {
             var validationRule = new AtLeastNCharacterValidationRule()
             {
@@ -45,6 +45,19 @@ namespace ZzaDesktopTests
             ValidationResult result = validationRule.Validate(null, CultureInfo.CurrentCulture);
             Assert.IsTrue(result.IsValid);
         }
+        
+        [TestMethod]
+        public void AtLeastNCharacterValidationRulePassesWithNullValueWhenZeroCharsIsValid()
+        {
+            var validationRule = new AtLeastNCharacterValidationRule()
+            {
+                MinimumStringLength = 4,
+                ZeroCharactersIsValid = true
+            };
+
+            ValidationResult result = validationRule.Validate(null, CultureInfo.CurrentCulture);
+            Assert.IsTrue(result.IsValid);
+        }
 
         [TestMethod]
         public void AtLeastNCharacterValidationRulePassesWithLongEnoughString()
@@ -52,6 +65,19 @@ namespace ZzaDesktopTests
             var validationRule = new AtLeastNCharacterValidationRule()
             {
                 MinimumStringLength = 4
+            };
+
+            ValidationResult result = validationRule.Validate("1234", CultureInfo.CurrentCulture);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void AtLeastNCharacterValidationRulePassesWithLongEnoughStringWhenZeroCharsIsValid()
+        {
+            var validationRule = new AtLeastNCharacterValidationRule()
+            {
+                MinimumStringLength = 4,
+                ZeroCharactersIsValid = true
             };
 
             ValidationResult result = validationRule.Validate("1234", CultureInfo.CurrentCulture);
@@ -71,11 +97,37 @@ namespace ZzaDesktopTests
         }
 
         [TestMethod]
+        public void AtLeastNCharacterValidationRulePassesWithLongStringWhenZeroCharsIsValid()
+        {
+            var validationRule = new AtLeastNCharacterValidationRule()
+            {
+                MinimumStringLength = 5,
+                ZeroCharactersIsValid = true
+            };
+
+            ValidationResult result = validationRule.Validate("1234567891011121314151617181920", CultureInfo.CurrentCulture);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
         public void AtLeastNCharacterValidationRuleFailsWithShortString()
         {
             var validationRule = new AtLeastNCharacterValidationRule()
             {
                 MinimumStringLength = 4
+            };
+
+            ValidationResult result = validationRule.Validate("123", CultureInfo.CurrentCulture);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void AtLeastNCharacterValidationRuleFailsWithShortStringWhenZeroCharsIsValid()
+        {
+            var validationRule = new AtLeastNCharacterValidationRule()
+            {
+                MinimumStringLength = 4,
+                ZeroCharactersIsValid = true
             };
 
             ValidationResult result = validationRule.Validate("123", CultureInfo.CurrentCulture);
